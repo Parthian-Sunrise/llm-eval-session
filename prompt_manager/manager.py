9dbf7886b703196b07f1e26834caab1b04f7098e
parthian-sunrise <jude.king@purplesmash.com>
2024-10-21 17:21:28 +0100

    feat: update line length

--- prompt_manager/manager.py
+++ prompt_manager/manager.py
@@ -1,4 +1,38 @@
 class PromptManager:
-    def __init__(self, template, placeholders=[]):
+    def __init__(self, template, inputs={}):
         self.template = template
-        self.placeholders = placeholders
+        self.inputs = inputs
+        self.formatted = False
+
+    def validate_inputs(self):
+        if self.formatted:
+            print("Already formatted, cannot validate")
+        else:
+            assertion_list = [
+                True if key in self.template else False for key in self.inputs.keys()
+            ]
+
+            if sum(assertion_list) == len(assertion_list):
+                print("Inputs validated successfully!")
+                self.validated = True
+            else:
+                print(
+                    "Inputs validation unsucessful! Check that all inputs are present in the prompt template"
+                )
+                self.validated = False
+                return None
+
+    def format_inputs(self):
+        if self.validated:
+            print("Previous validation successful")
+        else:
+            print("Validating inputs")
+            self.validate_inputs()
+        if self.formatted:
+            print("Already formatted")
+        else:
+            self.formatted = True
+            self.prompt = self.template
+            for key in self.inputs:
+                self.prompt = self.prompt.replace(key, self.inputs[key])
+            return self.prompt
