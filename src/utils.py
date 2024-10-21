a179dc9f92892528f7419a1656d92a427f96c8fc
parthian-sunrise <jude.king@purplesmash.com>
2024-10-21 16:28:36 +0100

    feat: test root function added

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import os
 from local_variables import ROOT_DIR
 
+
 def get_root_dir():
     return ROOT_DIR
+
+
+def test_root_dir(rel_root):
+    cwd = os.getcwd()
+    if cwd.replace(ROOT_DIR, "").count("/") == rel_root.count("/"):
+        print("Root set up correctly")
+    else:
+        print("Root may be incorrect, check local variables")
