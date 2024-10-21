af9c9274313c4f0e9855cade30c01cc93e55d334
parthian-sunrise <jude.king@purplesmash.com>
2024-10-21 15:18:40 +0100

    feat: initial prompt structure

---
+++ prompts/prompt_versions.py
@@ -0,0 +1,6 @@
+prompt_versions = {"task_1" :
+                   {"hallucination_detector" : "1"},
+                   "task_2" :
+                   {"bias_detector" : "1"},
+                   "task_3" :
+                   {"metric_1" : "1", "metric_2" : "2"}}
