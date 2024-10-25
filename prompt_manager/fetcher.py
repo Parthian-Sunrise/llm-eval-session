a111454f32b2770caa2c5b3a0ed0c8b8a0b09c7f
parthian-sunrise <jude.king@purplesmash.com>
2024-10-25 16:35:40 +0100

    feat: simplified and removed need for prompt_versions.py

--- prompt_manager/fetcher.py
+++ prompt_manager/fetcher.py
@@ -3,65 +3,33 @@ from local_variables import ROOT_DIR
 import importlib.util
 
 
-def fetch_prompt_version(version_dict, sequence):
-    """
-    Gets the latest prompt version using the specified sequence of identifiers for a given prompt i.e. version_dict = {'task_1' : {'metric_type_1' : {'sub_metric' : 1, 'sub_metric_2' : 11}}} then to get the correct version for sub_metric_2 we pass the sequence ["task_1","metric_type_1","sub_metric_2"]
-
-    Args:
-        version_dict (dict): dictionary matching prompts' file structure where the terminal values are the latest version for a given prompt
-        sequence (list): list of ordered keys that identify a specific prompt and its latest version using the version_dict dictionary
-    Returns:
-        version (str): str identifying the latest version of a prompt
-    """
-
-    # Loop through identifiers in the sequence
-    for index, identifier in enumerate(sequence):
-
-        # Different logic for the first identifier as we have yet to subset the dictionary
-        if index == 0:
-            subsetted_dict = version_dict
-            assert (
-                identifier in subsetted_dict.keys()
-            ), "1st element in identifying sequence was  not found in the prompt version dictionary"
-            subsetted_dict = subsetted_dict[identifier]
-        else:
-            assert (
-                identifier in subsetted_dict.keys()
-            ), f"Sequence element with index {index} not found in the prompt version dictionary at the indicated level"
-            subsetted_dict = subsetted_dict[identifier]
-
-        # subsetted_dict should be a decreasing tree of dictionaries until the final value at index=len(sequence)
-        if index < len(sequence) - 1:
-            assert (
-                type(subsetted_dict) is dict
-            ), "Prompt version dictionary structure does not match the length of the prompt identifying sequence"
-        else:
-            version = subsetted_dict
-            return version
-
-
 def fetch_prompt(
-    version_dict, sequence, root_directory=ROOT_DIR, prompt_folder="prompts"
+    sequence,
+    version="1",
+    root_directory=ROOT_DIR,
+    prompt_folder="prompts",
+    use_latest_version=False,
 ):
     """
     Fetches a prompt based on its latest version, identifying sequence and the directory set up
 
     Args:
-        version_dict (dict): dictionary matching prompts' file structure where the terminal values are the latest version for a given prompt
-        sequence (list): list of ordered keys that identify a specific prompt and its latest version using the version_dict dictionary, should match the prompts folder file structure
+        version (str): version identifying which of a given metric's prompts to fetch
+        sequence (list): list of ordered keys that identify a specific prompt type and its latest version using the version, should match the prompts folder file structure
         root_directory (str): absolute path of the root directory, assumed to be the local variable specified in the repo
         prompt_folder (str): assumed to be directly inside the root_directory and named 'prompts'
+        use_latest_version (bool): use largest (numerically) prompt version found in the prompts dictionary, assumes the versions are either integers or integers of strings otherwise the order is ambiguous
     Returns:
         latest_prompt (str): latest prompt identified by the sequence and version dictionary
     """
-    # Fetch version for prompt
-    version = fetch_prompt_version(version_dict, sequence)
 
     # Create path to prompt using sequencing
     path_to_prompt = os.path.join(root_directory, prompt_folder, sequence[0])
 
     # Assert that the .py file containing the prompt is named appropriately and in the right directory
-    assert f"{sequence[-1]}.py" in os.listdir(path_to_prompt)
+    assert f"{sequence[-1]}.py" in os.listdir(
+        path_to_prompt
+    ), f"{sequence[-1]}.py not found in {os.listdir(path_to_prompt)}"
 
     # Create path to the file containing prompts
     path_to_file = path_to_prompt + f"/{sequence[-1]}.py"
@@ -74,8 +42,32 @@ def fetch_prompt(
     # Access the metric's prompts
     metric_prompts = module.prompts
 
-    # Assert the latest version has an entry in the metric's prompts
-    assert version in metric_prompts.keys()
+    # Fetch latest version for prompt if specified
+    if use_latest_version:
+        # Identify whether keys are either integers or strings
+        all_int = all(isinstance(x, int) for x in metric_prompts.keys())
+        all_str = all(isinstance(x, str) for x in metric_prompts.keys())
+
+        # Assert it is either one or the other
+        assert (
+            all_int or all_str
+        ), "Version keys are not integers or string integers, cannot identify latest version. Please correct or specify the version manually using the version kwarg"
+
+        # Convert keys to integers to calculate maximum
+        version_int_list = [
+            int(key) if key is str else key for key in metric_prompts.keys()
+        ]
+        latest_version = max(version_int_list)
+
+        if all_str:
+            version = f"{latest_version}"
+        elif all_int:
+            version = latest_version
+    else:
+        # Assert the latest version has an entry in the metric's prompts
+        assert (
+            version in metric_prompts.keys()
+        ), f"Specified version not found in prompts dictionary at {path_to_file}"
 
     # Fetch the latest prompt
     latest_prompt = metric_prompts[version]
