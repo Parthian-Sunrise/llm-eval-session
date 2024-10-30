4d381982f8d3b84567f83b5f79b2ea4afab1d2f4
Jesslzy <126081495+Jesslzy@users.noreply.github.com>
2024-10-30 21:24:31 +0000

    feat: add docstring for api functions and checks on token

--- src/api.py
+++ src/api.py
@@ -1,10 +1,36 @@
 from openai import OpenAI
 from local_variables import token
 
-client = OpenAI(api_key=token)
+
+def get_openai_client():
+    """
+    Validate OpenAI token and generate OpenAI client if valid token exists.
+
+    Returns:
+        client: OpenAI client
+    """
+
+    if token == "":
+        raise ValueError(
+            "Invalid OpenAI token. Please make sure you replace the placeholder with the actual token."
+        )
+    else:
+        client = OpenAI(api_key=token)
+        return client
 
 
-def generate_outputs_openai(system_prompt):
+def generate_outputs_openai(system_prompt: str = None):
+    """
+    Generate OpenAI response with given system prompt.
+
+    Args:
+        system_prompt: system prompt used to generate OpenAi response.
+
+    Returns:
+        response: OpenAI response
+    """
+
+    client = get_openai_client()
 
     response = client.chat.completions.create(
         model="gpt-4o",
